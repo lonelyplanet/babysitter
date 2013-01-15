454c13cf9e7b03816e3a7e6750a400e1266c9a64
Nicolas Overloop <noverloop@gmail.com>
2013-01-15 12:12:30 +0000

    removed unused method

--- lib/babysitter/logging.rb
+++ lib/babysitter/logging.rb
@@ -1,15 +1,6 @@
 module Babysitter
   module Logging
 
-    def log_file_exceptions(file, &block)
-      begin
-        block.call(file)
-      rescue Exception
-        logger.error "Exception processing file #{file}"
-        raise
-      end
-    end
-
     def logger
       Babysitter.logger
     end
